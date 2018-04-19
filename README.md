b64852bc8c708efbc435a8c7829011fb695cac22
themaskbearer <themaskbearer@gmail.com>
2018-04-19 12:52:30 -0400

    basic for and if fixing implemented
    Implemented the ability for detection of for's and if's and to make sure that a space is added between the command and the '('
    
    - also create test directory
    - added readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # code-formatter
+
+This program is intended to take C++ code and format it to the desired format.
+
+Currently the program is very small and specific to my scenario (i.e. only works on Mercurial patches)
+but I intended to make it more generic.
+
+Would like to support user-defined configurations eventually
--- code-formatter.py
+++ code-formatter.py
@@ -1,13 +1,41 @@
-import io
-import sys
 
-# start of the program
+import sys
+import tempfile
+import os
+
+
+def replace_text(line, str_to_replace, replacement):
+    length = len(str_to_replace)
+    location = line.find(str_to_replace)
+
+    edited_line = line[:location] + replacement + line[location + length:]
+    return edited_line
+
+
+# start of the main path of execution
 
 if len(sys.argv) < 2:
-    print("Usage: code-formatter.py <patch name>")
+    print("Usage: code-formatter.py <file name>")
     exit(1)
 
 filename = sys.argv[1]
 
-with open(filename, 'r+') as file:
-    print(file.readline())
+with open(filename, 'r') as file, tempfile.NamedTemporaryFile(mode='w', dir='.', delete=False) as fixed_file:
+
+    for line in file:
+        # Only operate on lines that have been modified
+        if line[0] == "+":
+
+            if "for(" in line:
+                fixed_line = replace_text(line, "for(", "for (")
+                fixed_file.write(fixed_line)
+                continue
+
+            if "if(" in line:
+                fixed_line = replace_text(line, "if(", "if (")
+                fixed_file.write(fixed_line)
+                continue
+
+        fixed_file.write(line)
+
+os.replace(fixed_file.name, filename)
